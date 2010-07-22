8b0176ca4712e53f0cd31401e8a4735a1183d7e1
Kartik Thakore <kthakore@cpan.org>
2010-07-22 18:09:11 +0000

    Import of KTHAKORE/SDL-2.503 from CPAN.
    gitpan-cpan-distribution: SDL
    gitpan-cpan-version:      2.503
    gitpan-cpan-path:         KTHAKORE/SDL-2.503.tar.gz
    gitpan-cpan-author:       KTHAKORE
    gitpan-cpan-maturity:     released

--- examples/SDLx/SDLx_sprite_animated.pl
+++ examples/SDLx/SDLx_sprite_animated.pl
@@ -3,41 +3,39 @@ use SDL;
 use SDL::Video;
 use SDL::Color;
 use SDL::Rect;
-use SDL::Surface;
-use SDL::GFX::Rotozoom;
 
 use SDLx::Sprite::Animated;
 
-
-
 SDL::init(SDL_INIT_VIDEO);
 
-my $disp = SDL::Video::set_video_mode( 300, 300, 32, SDL_ANYFORMAT);
+my $disp = SDL::Video::set_video_mode( 300, 300, 32, SDL_ANYFORMAT );
 
-my $pixel   = SDL::Video::map_RGB( $disp->format, 0, 0, 0 );
-SDL::Video::fill_rect( $disp, SDL::Rect->new( 0, 0, $disp->w, $disp->h ), $pixel );
+my $pixel = SDL::Video::map_RGB( $disp->format, 0, 0, 0 );
+SDL::Video::fill_rect( $disp, SDL::Rect->new( 0, 0, $disp->w, $disp->h ),
+    $pixel );
 
+my $sprite = SDLx::Sprite::Animated->new(
+    image           => 'test/data/hero.png',
+    rect            => SDL::Rect->new( 48, 0, 48, 48 ),
+    ticks_per_frame => 6,
+);
+$sprite->set_sequences( left => [ [ 1, 0 ], [ 1, 1 ], [ 1, 2 ] ], );
+$sprite->alpha_key( SDL::Color->new( 0xfc, 0x00, 0xff ) );
+$sprite->sequence('left');
+$sprite->start();
+my $x     = 0;
+my $ticks = 0;
 
-my $sprite = SDLx::Sprite::Animated->new;
+while ( $x++ < 30 ) {
+    SDL::Video::fill_rect( $disp, SDL::Rect->new( 0, 0, $disp->w, $disp->h ),
+        $pixel );
 
- $sprite->load('test/data/hero.png');
- $sprite->alpha_key(SDL::Color->new(0xfc, 0x00, 0xff));
- $sprite->clip( SDL::Rect->new(48,0,48,48) );
- $sprite->step_y(48);
+    $sprite->x( $x * 10 );
+    $sprite->next();
+    $sprite->draw($disp);
 
- $sprite->y(150);
+    SDL::Video::update_rect( $disp, 0, 0, 0, 0 );
 
- my $x = 0; my $ticks = 0;
- while ($x++ < 30) {
-   SDL::Video::fill_rect( $disp, SDL::Rect->new( 0, 0, $disp->w, $disp->h ), $pixel );
-
-     $sprite->x($x * 10);
-     $sprite->draw($disp);
-
-     $sprite->next;
-
-     SDL::Video::update_rect($disp, 0, 0, 0, 0);
-
-     SDL::delay( 100 );
- }
+    SDL::delay(100);
+}
 
