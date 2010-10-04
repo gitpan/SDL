c3e37cd1c8583c3e8d97cffebc3761d56797fa3c
Kartik Thakore <kthakore@cpan.org>
2010-10-04 19:18:11 +0000

    Import of KTHAKORE/SDL-2.518 from CPAN.
    gitpan-cpan-distribution: SDL
    gitpan-cpan-version:      2.518
    gitpan-cpan-path:         KTHAKORE/SDL-2.518.tar.gz
    gitpan-cpan-author:       KTHAKORE
    gitpan-cpan-maturity:     released

--- lib/SDLx/Controller.pm
+++ lib/SDLx/Controller.pm
@@ -2,42 +2,44 @@ package SDLx::Controller;
 use strict;
 use warnings;
 use Carp;
-use Time::HiRes qw( time sleep );
+use Time::HiRes;
 use SDL;
 use SDL::Event;
 use SDL::Events;
 use SDL::Video;
-use SDLx::Controller::Timer;
-use Scalar::Util 'refaddr';
 use SDLx::Controller::Interface;
 use SDLx::Controller::State;
+use Scalar::Util 'refaddr';
 
 # inside out, so this can work as the superclass of another
 # SDL::Surface subclass
-my %_delta;
 my %_dt;
-my %_quit;
+my %_min_t;
+my %_current_time;
+my %_stop;
 my %_event;
 my %_event_handlers;
 my %_move_handlers;
 my %_show_handlers;
 
 sub new {
-	my $self = shift;
-	my $class = ref $self || $self;
-	my $a;
-	$self = ref $self ? $self : \$a;
-	bless $self, $class;
-
-	my %args = @_;
-	$_delta{ refaddr $self} = SDLx::Controller::Timer->new();
-	$_delta{ refaddr $self}->start(); # should do this after on_load
-	$_dt{ refaddr $self}             = $args{dt} || 0.1;
-	$_quit{ refaddr $self}           = $args{quit};
-	$_event{ refaddr $self}          = $args{event};
-	$_event_handlers{ refaddr $self} = $args{event_handlers};
-	$_move_handlers{ refaddr $self}  = $args{move_handlers};
-	$_show_handlers{ refaddr $self}  = $args{show_handlers};
+	my ($self, %args) = @_;
+	if(ref $self) {
+		bless $self, ref $self;
+	}
+	else {
+		my $a;
+		$self = bless \$a, $self;
+	}
+	
+	$_dt{ refaddr $self}                 = defined $args{dt}    ? $args{dt}    : 0.1;
+	$_min_t{ refaddr $self}              = defined $args{min_t} ? $args{min_t} : 1 / 60;
+#	$_current_time{ refaddr $self}       = $args{current_time} || 0; #no point
+	$_stop{ refaddr $self}               = $args{stop};
+	$_event{ refaddr $self}              = $args{event} || SDL::Event->new();
+	$_event_handlers{ refaddr $self}     = $args{event_handlers};
+	$_move_handlers{ refaddr $self}      = $args{move_handlers};
+	$_show_handlers{ refaddr $self}      = $args{show_handlers};
 
 	return $self;
 }
@@ -45,9 +47,10 @@ sub new {
 sub DESTROY {
 	my $self = shift;
 
-	delete $_delta{ refaddr $self};
 	delete $_dt{ refaddr $self};
-	delete $_quit{ refaddr $self};
+	delete $_min_t{ refaddr $self};
+	delete $_current_time{ refaddr $self};
+	delete $_stop{ refaddr $self};
 	delete $_event{ refaddr $self};
 	delete $_event_handlers{ refaddr $self};
 	delete $_move_handlers{ refaddr $self};
@@ -55,61 +58,78 @@ sub DESTROY {
 }
 
 sub run {
-	my $self         = shift;
-	my $quit         = 0;
-	my $t            = 0.0;
+	my ($self)       = @_;
 	my $dt           = $_dt{ refaddr $self};
-	my $current_time = 0.0;
-	my $accumulator  = 0.0;
-	while ( !$_quit{ refaddr $self} ) {
+	my $min_t        = $_min_t{ refaddr $self};
+	my $t            = 0.0;
+	$_current_time{ refaddr $self} = Time::HiRes::time;
+	while ( !$_stop{ refaddr $self} ) {
 		$self->_event;
 
-		my $new_time   = time;
-		my $delta_time = $new_time - $current_time;
-		$current_time = $new_time;
-		$delta_time = 0.25 if ( $delta_time > 0.25 );
-		$accumulator += $delta_time;
-		while ( $accumulator >= $dt ) {
-			$accumulator -= $dt;
-			$self->_move( $dt, $t );
+		my $new_time   = Time::HiRes::time;
+		my $delta_time = $new_time - $_current_time{ refaddr $self};
+		next if $delta_time < $min_t;
+		$_current_time{ refaddr $self} = $new_time;
+		my $delta_copy = $delta_time;
+
+		while ( $delta_copy > $dt ) {
+			$self->_move( 1, $t ); #a full move
+			$delta_copy -= $dt;
 			$t += $dt;
 		}
-		$self->_show( $accumulator / $dt );
+		my $step = $delta_copy / $dt;
+		$self->_move( $step, $t ); #a partial move
+		$t += $dt * $step;
+		
+		$self->_show( $delta_time );
+		
+		$dt    = $_dt{ refaddr $self};    #these can change
+		$min_t = $_min_t{ refaddr $self}; #during the cycle
 	}
 
 }
 
+sub pause {
+	my ($self, $callback) = @_;
+	$callback ||= sub {1};
+	my $event = SDL::Event->new();
+	while(1) {
+		SDL::Events::wait_event($event) or Carp::confess("pause failed waiting for an event");
+		if($callback->($event, $self)) {
+			$_current_time{ refaddr $self} = Time::HiRes::time; #so run doesn't catch up with the time paused
+			last;
+		}
+	}
+}
+
 sub _event {
-	my $self = shift;
-	$_event{ refaddr $self} = SDL::Event->new() unless $_event{ refaddr $self};
+	my ($self) = shift;
 	while ( SDL::Events::poll_event( $_event{ refaddr $self} ) ) {
 		SDL::Events::pump_events();
 		foreach my $event_handler ( @{ $_event_handlers{ refaddr $self} } ) {
-			$self->quit unless $event_handler->( $_event{ refaddr $self} );
+			next unless $event_handler;
+			$event_handler->( $_event{ refaddr $self}, $self );
 		}
 	}
 }
 
 sub _move {
-	my $self        = shift;
-	my $delta_ticks = shift;
-	my $t           = shift;
+	my ($self, $move_portion, $t) = @_;
 	foreach my $move_handler ( @{ $_move_handlers{ refaddr $self} } ) {
-		$move_handler->( $delta_ticks, $t );
+		next unless $move_handler;
+		$move_handler->( $move_portion, $self, $t );
 	}
 }
 
 sub _show {
-	my $self        = shift;
-	my $delta_ticks = shift;
-	foreach my $event_handler ( @{ $_show_handlers{ refaddr $self} } ) {
-		$event_handler->($delta_ticks);
+	my ($self, $delta_ticks) = @_;
+	foreach my $show_handler ( @{ $_show_handlers{ refaddr $self} } ) {
+		next unless $show_handler;
+		$show_handler->( $delta_ticks, $self );
 	}
 }
 
-sub quit { $_quit{ refaddr $_[0] } = 1 }
-
-
+sub stop { $_stop{ refaddr $_[0] } = 1 }
 
 sub _add_handler {
 	my ( $arr_ref, $handler ) = @_;
@@ -119,35 +139,38 @@ sub _add_handler {
 
 sub add_move_handler {
 	$_[0]->remove_all_move_handlers if !$_move_handlers{ refaddr $_[0] };
-	return _add_handler( $_move_handlers{ refaddr $_[0] }, $_[1] );
+	return _add_handler( $_move_handlers{ refaddr $_[0]}, $_[1] );
 }
 
 sub add_event_handler {
 	Carp::confess 'SDLx::App or a Display (SDL::Video::get_video_mode) must be made'
 		unless SDL::Video::get_video_surface();
 	$_[0]->remove_all_event_handlers if !$_event_handlers{ refaddr $_[0] };
-	return _add_handler( $_event_handlers{ refaddr $_[0] }, $_[1] );
+	return _add_handler( $_event_handlers{ refaddr $_[0]}, $_[1] );
 }
 
 sub add_show_handler {
 	$_[0]->remove_all_show_handlers if !$_show_handlers{ refaddr $_[0] };
-	return _add_handler( $_show_handlers{ refaddr $_[0] }, $_[1] );
+	return _add_handler( $_show_handlers{ refaddr $_[0]}, $_[1] );
 }
 
 sub _remove_handler {
-	my ( $handlers_ref, $id ) = @_;
+	my ( $arr_ref, $id ) = @_;
 	if ( ref $id ) {
-		$id = (
-			grep {
-				$id eq ${$handlers_ref}[$_] #coderef matches with input
-				} 0 .. $#{$handlers_ref}
-		)[0];                               #only the first coderef
+		($id) = grep { 
+					$id eq $arr_ref->[$_]
+				} 0..$#{$arr_ref};
+				
 		if ( !defined $id ) {
 			Carp::cluck("$id is not currently a handler of this type");
 			return;
 		}
 	}
-	return splice( @{$handlers_ref}, $id, 1 );
+	elsif(!defined $arr_ref->[$id]) {
+		Carp::cluck("$id is not currently a handler of this type");
+		return;
+	}
+	return delete( $arr_ref->[$id] );
 }
 
 sub remove_move_handler {
@@ -183,6 +206,30 @@ sub remove_all_show_handlers {
 sub move_handlers  { $_move_handlers{ refaddr $_[0] } }
 sub event_handlers { $_event_handlers{ refaddr $_[0] } }
 sub show_handlers  { $_show_handlers{ refaddr $_[0] } }
+
+sub dt {
+	my ($self, $arg) = @_;
+	
+	$_dt{ refaddr $self} = $arg if defined $arg;
+	
+	$_dt{ refaddr $self};
+}
+
+sub min_t {
+	my ($self, $arg) = @_;
+	
+	$_min_t{ refaddr $self} = $arg if defined $arg;
+	
+	$_min_t{ refaddr $self};
+}
+
+sub current_time {
+	my ($self, $arg) = @_;
+	
+	$_current_time{ refaddr $self} = $arg if defined $arg;
+	
+	$_current_time{ refaddr $self};
+}
 
 1; #not 42 man!
 
--- t/sdlx_controller.t
+++ t/sdlx_controller.t
@@ -7,11 +7,17 @@ use SDL::Video;
 use SDL::Color;
 use SDLx::Controller;
 use lib 't/lib';
-use SDL::TestTool;
+# use SDL::TestTool;
 
 can_ok(
 	'SDLx::Controller',
-	qw( new ) #meh, put the rest in later
+	qw(
+		new run stop pause dt min_t current_time
+		add_move_handler add_event_handler add_show_handler
+		remove_move_handler remove_event_handler remove_show_handler
+		remove_all_move_handlers remove_all_event_handlers remove_all_show_handlers
+		move_handlers event_handlers show_handlers
+	)
 );
 
 TODO: {
@@ -20,18 +26,42 @@ TODO: {
 }
 
 
-my $app = SDLx::Controller->new();
+my $app = SDLx::Controller->new(
+	dt     => 0.1,
+	min_t => 0.5,
+);
 
 isa_ok( $app, 'SDLx::Controller' );
 
-sub dummy_sub {1}
+sub dummy_sub  {1}
+sub dummy_sub2 {1}
 
-is( $app->add_move_handler( \&dummy_sub ), 0, 'index got from added handler' );
+my $index_1 = $app->add_move_handler( \&dummy_sub );
+my $index_2 = $app->add_move_handler( \&dummy_sub2 );
+
+is($index_1 , 0, 'index got from added handler' );
+is($index_2 , 1, 'index got from added handler' );
 
 is( $app->move_handlers->[0], \&dummy_sub, 'handler added correctly' );
+is( $app->move_handlers->[1], \&dummy_sub2, 'handler added correctly' );
 
 $app->remove_move_handler( \&dummy_sub );
+$app->remove_move_handler( $index_2 );
 
-is( scalar @{ $app->move_handlers }, 0, 'handler removed with coderef' );
+is( scalar @{ $app->move_handlers }, 0, 'handlers removed correctly' );
+
+sub test_move {
+	my $part = shift;
+	ok( do {$part > 0 and $part <= 1}, "move handle \$_[0] of $part was > 0 and <= 1" );
+}
+sub test_show {
+	my $ticks = shift;
+	ok( $ticks >= 0.5, "show handle \$_[0] of $ticks was >= 0.5" );
+	$app->stop();
+}
+
+$app->add_move_handler(\&test_move);
+$app->add_show_handler(\&test_show);
+$app->run();
 
 done_testing;
